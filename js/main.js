b7f65b3296d6f47a22044a488752faec91108e29
gerbertwest <gerbert.west@gmail.com>
2022-10-03 12:16:10 +0300

    Исправлены замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,38 +1,26 @@
-function randomIntegerInclusive(min, max) {
+function getRandomIntegerInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (typeof min !== 'number' || typeof max !== 'number') {
-    return NaN;
-  }
-  if (min < 0 || max < 0 || max === min) {
+  if (min < 0 || max < 0) {
     return NaN;
   }
   if (min > max) {
-    return randomIntegerInclusive(max, min);
-    /*const reverse = min;
-    min = max;
-    max = reverse;*/
+    return getRandomIntegerInclusive(max, min);
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-//Максимум и минимум включаются. Источник:https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-randomIntegerInclusive(1,10);
 
-function randomIntegerFloat(min, max, precision) {
+getRandomIntegerInclusive(1,10);
 
-  if (typeof min !== 'number' || typeof max !== 'number') {
+function getRandomIntegerFloat(min, max, precision = 1) {
+
+  if (min < 0 || max < 0) {
     return NaN;
   }
-  if (min < 0 || max < 0 || max === min) {
-    return NaN;
-  }
-  if (max < min) {
-    const reverse = min;
-    min = max;
-    max = reverse;
+  if (min > max) {
+    return getRandomIntegerFloat(max, min);
   }
   return +(Math.floor(Math.random() * (max - min + 1)) + min).toFixed(precision);
 }
-//Максимум и минимум включаются. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-//Метод toFixed. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-randomIntegerFloat (1.152,10.569,1);
+
+getRandomIntegerFloat(1.152,10.569,2);
