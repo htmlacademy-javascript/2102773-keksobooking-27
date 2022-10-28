f89e82b521a87eb0bf0d07b85b4f9c675558c538
gerbertwest <gerbert.west@gmail.com>
2022-10-28 23:44:35 +0300

    Правда или действие (часть 2)

--- js/notice-form.js
+++ js/notice-form.js
@@ -19,7 +19,7 @@ function validateRooms () {
   else {return roomsField.value >= 100 && capacityField.value <= 0;}
 }
 
-function gerRoomsErrorMassage () {
+function gerRoomsErrorMessage () {
   let errorText;
   if (roomsField.value < 100 && capacityField.value > 0) {
     errorText = `Не более ${roomsField.value} ${roomsField.value <= 1 ? 'гостя' : 'гостей'}`;
@@ -33,7 +33,37 @@ function gerRoomsErrorMassage () {
   return errorText;
 }
 
-pristine.addValidator(capacityField, validateRooms, gerRoomsErrorMassage);
+pristine.addValidator(capacityField, validateRooms, gerRoomsErrorMessage);
+
+const timein = noticeForm.querySelector('#timein');
+const timeout = noticeForm.querySelector('#timeout');
+
+timein.addEventListener('change', () =>{timeout.value = timein.value;});
+timeout.addEventListener('change', () => {timein.value = timeout.value;});
+
+const priceField = noticeForm.querySelector('#price');
+const minPrice = {
+  'bungalow' : 0,
+  'flat' : 1000,
+  'hotel' : 3000,
+  'house' : 5000,
+  'palace' : 10000
+};
+const typeField = noticeForm.querySelector('#type');
+
+typeField.addEventListener('change', () => {
+  priceField.placeholder = minPrice[typeField.value];
+  priceField.min = minPrice[typeField.value];
+});
+
+function validatePrice () {
+  return priceField.value >= Number(priceField.min);
+}
+function getPriceErrorMessage () {
+  return `Не менее ${priceField.min} ₽/ночь`;
+}
+
+pristine.addValidator(priceField, validatePrice, getPriceErrorMessage);
 
 noticeForm.addEventListener('submit', (evt) => {
   const isValid = pristine.validate();
